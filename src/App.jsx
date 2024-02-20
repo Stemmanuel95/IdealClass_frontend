82f6ce80bebc2d8d98775c436d25d546e8f9eb90
Forster Asamany <asamanyforster@yoo.com>
2024-02-20 05:53:52 -0500

    build: react app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import React from "react";
+import Signup from "./components/signup";
+import Login from "./components/login";
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Signup/>
+      <Login/>
+    </div>
+    
   );
 };
 
