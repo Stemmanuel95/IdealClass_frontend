32f3c5b91794e94e0c14bde42dbd49e5c9ecd0c3
Forster Asamany <asamanyforster@yoo.com>
2024-02-20 23:07:56 -0500

    build: signup and login setup

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Signup = () => {
+const Login = () => {
   return (
     <div className="App">
-      <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <div className="flex items-center justify-center">
             <img
@@ -127,4 +127,4 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+export default Login;
--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Signup = () => {
   return (
     <div className="App">
-      <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <div className="flex items-center justify-center">
             <img
