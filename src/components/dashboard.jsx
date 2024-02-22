9d66cb9fe72e9b7a8aadcbb60036605ee3b99df3
Forster Asamany <asamanyforster@yoo.com>
2024-02-22 18:17:39 -0500

    build: pre-setup

---
+++ src/components/dashboard.jsx
@@ -0,0 +1,340 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <nav className="fixed top-0 z-50 w-full bg-white border-b border-gray-400 ">
+        <div className="px-3 py-4 lg:px-5 lg:pl-3">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center justify-start rtl:justify-end">
+              <button
+                data-drawer-target="logo-sidebar"
+                data-drawer-toggle="logo-sidebar"
+                aria-controls="logo-sidebar"
+                type="button"
+                className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-300 "
+              >
+                <span className="sr-only">Open sidebar</span>
+                <svg
+                  className="w-6 h-6"
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                    d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+                  ></path>
+                </svg>
+              </button>
+              <a href="/" className="flex ms-2 md:me-24">
+                <img
+                  src="./src/assets/iclogobg.png"
+                  className="h-8 me-3"
+                  alt="iclogo"
+                />
+              </a>
+              <span className="ml-3 self-center text-xl font-semibold sm:text-2xl whitespace-nowrap text-black">
+                Hello, Michael
+              </span>
+            </div>
+
+            <div className="flex items-center">
+              <div className="flex items-center ms-3">
+                <button
+                  className="mr-8"
+                  data-popover-target="popover-default"
+                  type="button"
+                >
+                  <svg
+                    class="w-6 h-6 text-gray-800 "
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M5 12h14m-7 7V5"
+                    />
+                  </svg>
+                </button>
+
+                <div>
+                  <button
+                    type="button"
+                    className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-400"
+                    aria-expanded="false"
+                    data-dropdown-toggle="dropdown-user"
+                  >
+                    <span className="sr-only">Open user menu</span>
+                    <img
+                      className="w-8 h-8 rounded-full"
+                      src="./src/assets/mike.jpeg"
+                      alt="user photo"
+                    />
+                  </button>
+                </div>
+                <div
+                  className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded shadow dark:bg-gray-700 dark:divide-gray-600"
+                  id="dropdown-user"
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <aside
+        id="logo-sidebar"
+        className="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-gray-200 border-r border-gray-200 sm:translate-x-0"
+        aria-label="Sidebar"
+      >
+        <div className="h-full px-3 pb-4 overflow-y-auto bg-gray-200">
+          <ul className="space-y-2 font-medium">
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-50 "
+              >
+                <svg
+                  className="w-5 h-5 text-gray-900 transition duration-75 group-hover:text-gray-900 "
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 22 21"
+                >
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                </svg>
+                <span className="ms-3">Dashboard</span>
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gray-50 group"
+              >
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-gray-900 transition duration-75  group-hover:text-gray-900 "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 18 18"
+                >
+                  <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
+                </svg>
+                <span className="flex-1 ms-3 whitespace-nowrap">Calendar</span>
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-50 group"
+              >
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-gray-900 transition duration-75 group-hover:text-gray-900 "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z" />
+                  <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
+                  <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
+                </svg>
+                <span className="flex-1 ms-3 whitespace-nowrap">Courses</span>
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-50 group"
+              >
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-gray-900 transition duration-75  group-hover:text-gray-900 "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z" />
+                </svg>
+                <span className="flex-1 ms-3 whitespace-nowrap">Inbox</span>
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="#"
+                className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-50 group"
+              >
+                <svg
+                  className="flex-shrink-0 w-5 h-5 text-gray-900 transition duration-75 group-hover:text-gray-900 "
+                  aria-hidden="true"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <span className="flex-1 ms-3 whitespace-nowrap">Settings</span>
+              </a>
+            </li>
+          </ul>
+
+          <ul className="mt-20 font-medium">
+            <li>
+              <a
+                href="#"
+                className=" flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-50 group"
+              >
+                <svg
+                  class="w-6 h-6 text-gray-800 "
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M20 12H8m12 0-4 4m4-4-4-4M9 4H7a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h2"
+                  />
+                </svg>
+
+                <span className="flex-1 ms-3 whitespace-nowrap">Log Out</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </aside>
+
+      <div className="p-4 sm:ml-64">
+        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg mt-14">
+          <div className="grid-cols-1 grid md:grid-cols-2 gap-10 p-10 ">
+            <div className="flex justify-center items-center border h-32 w-48 bg-blue-100 md:grid-cols-2 sm:grid-cols-1">
+              1
+            </div>
+
+            <div className="flex justify-center items-center border h-32 w-48 bg-blue-100 ">
+              2
+            </div>
+
+            <div className="flex justify-center items-center border h-32 w-48 bg-blue-100 ">
+              3
+            </div>
+
+            <div className="flex justify-center items-center border h-32 w-48 bg-blue-100 ">
+              4
+            </div>
+          </div>
+
+          <div className="relative overflow-x-auto">
+            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                <tr>
+                  <th scope="col" className="px-6 py-3">
+                    Name
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Coure
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Email
+                  </th>
+                  <th scope="col" className="px-6 py-3">
+                    Country
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="bg-white border-gray-700">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                  >
+                    John Doe
+                  </th>
+                  <td className="px-6 py-4">Course 1</td>
+                  <td className="px-6 py-4">user@email.com</td>
+                  <td className="px-6 py-4">Benin</td>
+                </tr>
+                <tr className="bg-white  border-gray-700">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                  >
+                    Mason White
+                  </th>
+                  <td className="px-6 py-4">Course 1</td>
+                  <td className="px-6 py-4">user@email.com</td>
+                  <td className="px-6 py-4">Nigeria</td>
+                </tr>
+                <tr className="bg-white border-gray-700">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                  >
+                    Joe White
+                  </th>
+                  <td className="px-6 py-4">Course 1</td>
+                  <td className="px-6 py-4">user@email.com</td>
+                  <td className="px-6 py-4">Ghana</td>
+                </tr>
+                <tr className="bg-white border-gray-700">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                  >
+                    Joe White
+                  </th>
+                  <td className="px-6 py-4">Course 1</td>
+                  <td className="px-6 py-4">user@email.com</td>
+                  <td className="px-6 py-4">Ghana</td>
+                </tr>
+                <tr className="bg-white border-gray-700">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                  >
+                    Joe White
+                  </th>
+                  <td className="px-6 py-4">Course 1</td>
+                  <td className="px-6 py-4">user@email.com</td>
+                  <td className="px-6 py-4">Ghana</td>
+                </tr>
+                <tr className="bg-white border-gray-700">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+                  >
+                    Joe White
+                  </th>
+                  <td className="px-6 py-4">Course 1</td>
+                  <td className="px-6 py-4">user@email.com</td>
+                  <td className="px-6 py-4">Ghana</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
